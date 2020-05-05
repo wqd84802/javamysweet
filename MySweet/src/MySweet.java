3df3f82ded34e9b94fca9ab0d0ec6c0457b92d92
wqd84802 <wqd84802@gmail.com>
2020-05-05 20:58:15 +0800

    init project

---
+++ MySweet/src/MySweet.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MySweet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+//		Scanner in = new Scanner(System.in);
+//		String s = in.nextLine();
+		double f = 5.5;
+		System.out.println(f);
+		for (int i = 0; i < 10; i++) {
+			System.out.println("i is "+i);
+		}
+	}
+
+}
